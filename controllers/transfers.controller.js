867b0f12fe61859ff1c94f6d293a62d10c90409d
Emmanuel D <informaticasaba@gmail.com>
2023-01-30 19:40:32 -0400

    segundo commit

--- controllers/transfers.controller.js
+++ controllers/transfers.controller.js
@@ -1,9 +1,12 @@
 const Transfer = require('../models/transfer.model');
 const User = require('../models/user.model');
 
+// funcion para realizar una transferencia y guardarla en la base de datos
 exports.transferAmount = async (req, res) => {
+  //1.Recibo el monto, el numero de cuenta del recibiendo y el usuario que va a mandar por el body
   const { amount, accountNumber, senderUserId } = req.body;
 
+  //2.Verifico si  existe un numero de cuenta igual a la que pase y al mismo tiempo el usuario recibiente existe a traves de su numero de cuenta
   const userReceiver = await User.findOne({
     where: {
       accountNumber,
@@ -11,6 +14,7 @@ exports.transferAmount = async (req, res) => {
     },
   });
 
+  //3. Si el usuario recibiente no existe mando un error y salgo de la funcion
   if (!userReceiver) {
     return res.status(404).json({
       status: 'error',
@@ -18,8 +22,10 @@ exports.transferAmount = async (req, res) => {
     });
   }
 
+  //4. Pongo en una variable el id del usuario recibiente
   const receiverUserId = userReceiver.id;
 
+  //5. A traves del id que recibo, verifiqo si existe un usuario igual con ese id
   const userSend = await User.findOne({
     where: {
       id: senderUserId,
@@ -27,6 +33,7 @@ exports.transferAmount = async (req, res) => {
     },
   });
 
+  //6. Si el usuario que manda no existe mando un error y salgo de la funcion
   if (!userSend) {
     return res.status(404).json({
       status: 'error',
@@ -34,6 +41,7 @@ exports.transferAmount = async (req, res) => {
     });
   }
 
+  //7. Si el monto es mayor a lo que tiene la cuenta mando un error y salgo de la funcion
   if (amount > +userSend.amount) {
     return res.status(404).json({
       status: 'error',
@@ -41,27 +49,35 @@ exports.transferAmount = async (req, res) => {
     });
   }
 
+  //8. Verifico si el usuario que manda es quien recibe y si es asi mando un error y salgo de la funcion
   if (userReceiver.id == userSend.id) {
     return res.status(404).json({
       status: 'error',
       message: 'you cannot send yourself',
     });
   }
+
+  //9. Pongo en una constante la resta de lo que se manda - los que tiene la cuenta
   const newAmountUserMakeTransfer = userSend.amount - amount;
+  //10. Sumo lo que recibo con lo que tiene en su cuenta el usuario reciente
   const newAmountUserReceiverTransfer = userReceiver.amount + amount;
 
+  //11. actualizo en la base de datos la resta del usuario enviante, y la suma del usuario reciente
   await userSend.update({
     amount: newAmountUserMakeTransfer,
   });
   await userReceiver.update({
     amount: newAmountUserReceiverTransfer,
   });
+
+  //13. Creo en la tabla de la base de datos un nuevo registro con la transacion  (el Monto, el id del usuario mandante y el id del usuario recibiente)
   const transfer = await Transfer.create({
     amount,
     senderUserId,
     receiverUserId,
   });
 
+  //13. Mando una repuesta de la transacion (el Monto, el id del usuario mandante y el id del usuario recibiente)
   res.status(201).json({
     status: 'success',
     message: 'The transfer was created successfully',
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,8 +1,11 @@
 const User = require('../models/user.model');
 
+// Funcion para logear a un usuario
 exports.login = async (req, res) => {
+  //1. Recibo de req.body la clave y numero de cuenta
   const { password, accountNumber } = req.body;
 
+  //2.Busco el usuario en mi base de datos
   const user = await User.findOne({
     where: {
       accountNumber,
@@ -10,13 +13,14 @@ exports.login = async (req, res) => {
       status: true,
     },
   });
-
+  //3.Verifico y se encuentra, sino mando un error
   if (!user) {
     return res.status(404).json({
       status: 'error',
       message: 'The user o password not exist',
     });
   }
+  //3.Si se encontro, mando un mensaje de logeado
   return res.status(200).json({
     status: 'success',
     message: 'User successfully logged in',
@@ -24,6 +28,7 @@ exports.login = async (req, res) => {
   });
 };
 
+// Funcion para registrar a un usuario
 exports.createUser = async (req, res) => {
   const { name, password } = req.body;
 
